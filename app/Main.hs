6f8368fd6359358af79e352cbcbd43707429c04b
Alexander Bernauer <acopton@gmail.com>
2020-05-06 22:42:44 +0200

    starting xml output generation

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,13 @@
 module Main where
 
-import Model
+import Text.XML.HaXml as X
+import Text.XML.HaXml.Pretty as P
 
 main :: IO ()
-main = undefined
+main = do
+    xml <- readFile "example.svg"
+    let d1 = X.xmlParse "example.svg" xml
+    let d2 = P.document d1
+    let d3 = X.render d2
+--    doc <- X.readFile def "example.svg"
+    putStr d3
--- src/SvgXml.hs
+++ src/SvgXml.hs
@@ -6,6 +6,37 @@ module SvgXml
 import qualified SvgElements as E
 
 import Data.Tagged (Tagged(Tagged, unTagged))
+import Text.XML.HaXml.Types
+import Text.XML.HaXml.Pretty (document)
+import Text.XML.HaXml (render)
+
+toXml :: [E.Element] -> String
+toXml = render . document . xml
+
+xml :: [E.Element] -> Document ()
+xml elements = Document prolog [] (root elements) []
+
+prolog :: Prolog
+prolog = Prolog (Just decl) [] (Just dtd) []
+    where
+        decl = XMLDecl "1.0" (Just (EncodingDecl "utf-8")) (Just False)
+        dtd = DTD (N "svg") (Just eid) []
+        eid = PUBLIC (PubidLiteral pl) (SystemLiteral sl)
+        pl = "-//W3C//DTD SVG 1.1//EN"
+        sl = "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd"
+
+root :: [E.Element] -> Element ()
+root elements = Elem (N "svg") attrs (content elements)
+    where
+        attrs = [width, height, vb, ns, version]
+        ns = (N "xmlns:xlink", AttValue [Left "http://www.w3.org/1999/xlink"])
+        vb = undefined
+        width = (N "width", AttValue [Left "100%"])
+        height = (N "height", AttValue [Left "100%"])
+        version = (N "version", AttValue [Left "1.1"])
+
+content :: [E.Element] -> [Content ()]
+content = undefined
 
 viewBox :: [E.Element] -> Int
 viewBox = maximum . concatMap viewBoxElement
